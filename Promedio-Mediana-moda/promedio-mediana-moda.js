1bb9bf1bd005ec114a7049be4ff36211f9037f8a
Jair Castañeda <jaiacastanedarod@unal.edu.co>
2022-05-14 16:30:10 -0500

    Se agregan las dos funciones para calcular el promedio artimetico y ponderado, estas funciones permiten recoger los datos del html, mostrarlos en un array y aperarlos para mostrar el resultado en el documento html a traves de una etiqueta div

--- Promedio-Mediana-moda/promedio-mediana-moda.js
+++ Promedio-Mediana-moda/promedio-mediana-moda.js
@@ -1,74 +1,58 @@
-// Función para el calculo del promedio aritmetico 
-
-// function capturaDatos(){
-//     let valor1 = document.getElementsByClassName("valor_aritmetico"),
-//     arrayGuardar = [];
-
-//     console.log(valor1[1].value);
-// }
-
-// https://www.youtube.com/watch?v=wJWjJWs81g4
-// https://es.stackoverflow.com/questions/154451/como-puedo-almacenar-datos-desde-un-formulario-a-un-array-en-javascript
-
-
-
-// function CalculoPromedioAritmetico() { 
-
-//     capturaDatos();
-
-//     let sumaLista = 0;
-
-//     for (let i = 0; i < lista1.length; i++) {
-//         sumaLista = sumaLista + lista1[i];
-//     }
-//     const promedio = sumaLista / lista1.length;
-//     return promedio; 
-
-//     // const resultadoAritmetico = document.getElementById("resultadoPromedioAritmetico");
-
-//     // resultadoAritmetico.innerText = promedio;
-
-
-// }
-
-
-function viewsArrayInput() {
-    var arrayInput = new Array();
-    var inputsValues = document.getElementsByClassName('valor_aritmetico'),
+// Función para calcular el promedio aritmetico
+function CalculoPromedioAritmetico() {
+    let arrayInput = new Array(); // Creamos un array vacio 
+    let inputsValues = document.getElementsByClassName('valor_aritmetico'), // Obtenemos los datos de los inputs
+        // Pasamos cada dato obtenido en inputsValues al array
         namesValues = [].map.call(inputsValues, function (dataInput) {
             arrayInput.push(Number(dataInput.value));
         });
 
-    arrayInput.forEach(function (inputsValuesData) {
-        // console.log ("El dato es:"  + arrayInput);
+    // Calculamos el promedio artimetico
+    let sumaLista = 0;  // Se crea una variable vacia
+    // Con este for sumamos cada elemento del array, recorremos el array y vamos sumando cada digito al acumulado
+    for (let i = 0; i < arrayInput.length; i++) { 
+        sumaLista += arrayInput[i];
+    }
+    // Dividimos la suma de todos los elementos de array sobre el total del elementos
+    const promedio = sumaLista / arrayInput.length;
+    // Obtenemos el elemento del html en donde mostraremos el resultado
+    const resultadoAritmetico = document.getElementById("resultadoPromedioAritmetico");
+    // Escribimos el resultado en el elemento que obtuvimos previamente
+    resultadoAritmetico.innerText = promedio;
 
-        // var lengthArray = arrayInput.length;
-
-        var sumaLista = 0;
-
-        for (let i = 0; i < arrayInput.length; i++) {
-            sumaLista = sumaLista + arrayInput[i];
-        }
-
-        const promedio = sumaLista / arrayInput.length;
-
-        const resultadoAritmetico = document.getElementById("resultadoPromedioAritmetico");
-
-        resultadoAritmetico.innerText = promedio;
-    })
 }
 
+//Función para calcular el promedio ponderado | La forma de obtener los datos en un array fue la misma que en la función anterior
+function CalculoPromedioPonderado() {
+    let datos = new Array();
+    let datosValue = document.getElementsByClassName('valor_ponderado'),
+        namesDatosValues = [].map.call(datosValue, function (datosInput) {
+            datos.push(Number(datosInput.value));
+        });
 
-
-function CalculoPromedioPonderado(lista, peso) {
-    let sumaLista = 0;
+    let peso = new Array();
+    let pesoValue = document.getElementsByClassName('peso'),
+        namesPesoValues = [].map.call(pesoValue, function (datapeso) {
+            peso.push(Number(datapeso.value));
+        });
+    // Tenemos 2 array, uno con los datos y otro con el peso de cada dato
+    
+    // Creamos 2 variables, una para la suma del peso de los valores y la otra para la suma de los datos
+    let pesoValor = 0;
     let sumaPeso = 0;
-
-    for (let i = 0; i < lista.length; i++) {
-        sumaLista = sumaLista + (lista[i] * peso[i]);
-        sumaPeso = sumaPeso + peso[i];
+    
+    /* Recorremos el array de peso y en cada elemento lo vamos a multiplicar por un elemento del valor, luego este se
+     sumara al anterior, tambien (Aprovechando que los dos array miden lo mismo), sumamos todos los datos de peso*/
+    for (i = 0; i < peso.length; i++) {
+        pesoValor += (peso[i] * datos[i]);
+        sumaPeso += peso[i];
     }
+    // dividimos suma de la multiplicación de cada uno de los datos por peso sobre la suma de todos los pesos
+    Ponderado = pesoValor / sumaPeso;
 
-    const promedioPonderado = sumaLista / sumaPeso;
-    return promedioPonderado;
+    // Obtenemos el elemento donde se va a mostrar la respuesta
+    const resultadoPonderado = document.getElementById("resultadoPromedioPonderado");
+    // Escribimos el resultado en el elemento 
+    resultadoPonderado.innerText = Ponderado;
+
 }
